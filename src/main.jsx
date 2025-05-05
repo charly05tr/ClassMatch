1c5ac68b5d5a8f4b86da51cb6f3e60d3f3dfb126
charl <charlyst23dv@gmail.com>
2025-05-05 02:07:50 -0600

    x

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
-
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-);
+  </StrictMode>,
+)
