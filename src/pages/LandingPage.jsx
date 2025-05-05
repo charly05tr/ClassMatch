91827964ffe695e931b80fd40b448b55383403fe
charl <charlyst23dv@gmail.com>
2025-05-05 00:05:22 -0600

    x

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -9,6 +9,9 @@ function LandingPage() {
     const goToRegister = () => {
         navigate('/register')
     }
+    const goToLogIn = () => {
+        navigate('/login')
+    }
      useEffect(() => {
         const checkLoginStatus = async () => {
           try {
@@ -40,7 +43,7 @@ function LandingPage() {
                 {/* Enlaces de Navegación (Placeholder) */}
                 <nav>
                     {/* Estos serían enlaces reales de tu router (ej. React Router Link) */}
-                    <a href="/login" className="text-gray-300 hover:text-white mr-4">Login</a>
+                    <button type="button" onClick={() => goToLogIn()} className="text-gray-300 hover:text-white mr-4">Login</button>
                     <button type='button'onClick={() => goToRegister()} className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Regístrate</button>
                 </nav>
             </header>
