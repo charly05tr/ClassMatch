6c29b9644048443771d4e78641b9a3826462bd7d
charl <charlyst23dv@gmail.com>
2025-05-11 12:44:22 -0600

    tailwind in production

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import flowbiteReact from "flowbite-react/plugin/vite";
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), flowbiteReact()],
+  plugins: [react(), flowbiteReact(), tailwindcss(),],
   build: {
     outDir: 'dist',
   },
